d4cd838799ca8446fc7797a167ca120ae8c3a0f9
Stiliyan Yankov <s.yankov@is-bg.net>
2024-07-09 19:39:15 +0300

    Initial Commit

---
+++ TrafficLights/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TrafficLights
+//
+//  Created by Stiliyan Yankov on 9.07.24.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Properties
+    var window: UIWindow?
+    
+    // MARK: - Application Delegate
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
